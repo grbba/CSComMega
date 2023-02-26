cde5195a6015eff28efb080ac8b5aa3bb3d219d8
Gregor Baues <gr2bba@gmail.com>
2023-02-26 13:07:16 +0100

    rdefines the queues as static and not allocated at setup

--- include/DccExInterface.h
+++ include/DccExInterface.h
@@ -1,6 +1,6 @@
 /**
  *  © 2023 Gregor Baues. All rights reserved.
- *  
+ *
  *  This is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -36,21 +36,21 @@
  */
 typedef enum
 {
-    _DCCSTA,        // CommandStation
-    _NWSTA,         // NetworkStation
-    _UNKNOWN_STA    // Unspecified -> don't know howto handle message
+    _DCCSTA,     // CommandStation
+    _NWSTA,      // NetworkStation
+    _UNKNOWN_STA // Unspecified -> don't know howto handle message
 } comStation;
 // static_cast enum to int is ok as enum is implemented as int
 // static_cast int to enum works as well but invalid enum values will be accepted so needs sanity check
 /**
- * @brief "Hardware" elated protocols to comuunicate with a connected MCU. As of now 
+ * @brief "Hardware" elated protocols to comuunicate with a connected MCU. As of now
  * only Serial is supported
  */
 typedef enum
 {
-    SRL,        // serial
-    I2C,        // i2c
-    SPI,        // spi
+    SRL, // serial
+    I2C, // i2c
+    SPI, // spi
     UNKNOWN_COM_PROTOCOL
 } comProtocol;
 /**
@@ -61,86 +61,88 @@ typedef enum
 #ifdef DCCI_CS
 typedef enum
 {
-    _DCCEX,          //< > encoded
-    _WITHROTTLE,     // Withrottle 
-    _REPLY,          // Message comming back from the commandstation after the execution of a command; all replys will be forwarded to the originating client
-    _DIAG,           // Diagnostic messages comming back from the commandstation
-    _MQTT,           // MQTT messages they are handled only on the NW station just like HTTP
-    _HTTP,           //  HTTP endpoint
-    _CTRL,           // to be set when sending to the CS if the the msg send starts with "<!" avoids the need to do the check on the CS
-    UNKNOWN_CS_PROTOCOL  // DO NOT remove; used for sizing and testing conditions
+    _DCCEX,             //< > encoded
+    _WITHROTTLE,        // Withrottle
+    _REPLY,             // Message comming back from the commandstation after the execution of a command; all replys will be forwarded to the originating client
+    _DIAG,              // Diagnostic messages comming back from the commandstation
+    _MQTT,              // MQTT messages they are handled only on the NW station just like HTTP
+    _HTTP,              //  HTTP endpoint
+    _CTRL,              // to be set when sending to the CS if the the msg send starts with "<!" avoids the need to do the check on the CS
+    UNKNOWN_CS_PROTOCOL // DO NOT remove; used for sizing and testing conditions
 } csProtocol;
 #endif
-#define HANDLERS  \
-    static void dccexHandler(DccMessage &m); \
+#define HANDLERS                                  \
+    static void dccexHandler(DccMessage &m);      \
     static void wiThrottleHandler(DccMessage &m); \
-    static void notYetHandler(DccMessage &m); \
-    static void replyHandler(DccMessage &m); \
-    static void diagHandler(DccMessage &m); \
+    static void notYetHandler(DccMessage &m);     \
+    static void replyHandler(DccMessage &m);      \
+    static void diagHandler(DccMessage &m);       \
     static void ctrlHandler(DccMessage &m);
 
 #ifndef DCCI_CS
-#define HANDLER_INIT  \
-   _tcsProtocolHandler handlers[UNKNOWN_CS_PROTOCOL] = \
-   {dccexHandler, notYetHandler, ctrlHandler, replyHandler, diagHandler, notYetHandler, notYetHandler}; 
+#define HANDLER_INIT                                    \
+    _tcsProtocolHandler handlers[UNKNOWN_CS_PROTOCOL] = \
+        {dccexHandler, notYetHandler, ctrlHandler, replyHandler, diagHandler, notYetHandler, notYetHandler};
 #else
-#define HANDLER_INIT  \
-   _tcsProtocolHandler handlers[UNKNOWN_CS_PROTOCOL] = \
-   {    dccexHandler,   notYetHandler,  notYetHandler,  notYetHandler,  notYetHandler,  notYetHandler,  ctrlHandler}; 
+#define HANDLER_INIT                                    \
+    _tcsProtocolHandler handlers[UNKNOWN_CS_PROTOCOL] = \
+        {dccexHandler, notYetHandler, notYetHandler, notYetHandler, notYetHandler, notYetHandler, ctrlHandler};
 #endif
 //      _DCCEX          _WITHROTTLE,    _REPLY        _DIAG           _MQTT           _HTTP           _CTRL
 
 /**
- * @brief DccMessage is the struct serailazed and send over the 
+ * @brief DccMessage is the struct serailazed and send over the
  *        wire to either the command or network station
- * 
+ *
  */
-class DccMessage {
+class DccMessage
+{
 public:
-    int sta;                    // station allowed values are comming from the comStation enum 
-                                // but as msgpack doesn't really work on enums(?) 
-    int mid;                    // message id; sequence number 
-    int client;                 // client id ( socket number from Wifi or Ethernet to be checked if we need to also have the original channel)
-    int p;                      // either JMRI or WITHROTTLE in order to understand the content of the msg payload
-    MsgPack::str_t msg;         // going to CS this is a command and a reply on return
+    int sta;            // station allowed values are comming from the comStation enum
+                        // but as msgpack doesn't really work on enums(?)
+    int mid;            // message id; sequence number
+    int client;         // client id ( socket number from Wifi or Ethernet to be checked if we need to also have the original channel)
+    int p;              // either JMRI or WITHROTTLE in order to understand the content of the msg payload
+    MsgPack::str_t msg; // going to CS this is a command and a reply on return
     MSGPACK_DEFINE(sta, mid, client, p, msg);
 
-    DccMessage() {
-        msg.reserve(MAX_MESSAGE_SIZE);        // reserve upfront space; requires that we check that no command exceeds MAX_MESSAGE_SIZE
-                                              // avoids heap memory fragmentation and the whole reciev send process runs in constant memory  
+    DccMessage()
+    {
+        msg.reserve(MAX_MESSAGE_SIZE); // reserve upfront space; requires that we check that no command exceeds MAX_MESSAGE_SIZE
+                                       // avoids heap memory fragmentation and the whole reciev send process runs in constant memory
     }
-}; 
+};
 
 typedef Queue<DccMessage, MAX_QUEUE_SIZE> _tDccQueue;
-using  _tcsProtocolHandler = void (*)(DccMessage &m);
+using _tcsProtocolHandler = void (*)(DccMessage &m);
 
 typedef enum
 {
-    IN,      
-    OUT,      
+    IN,
+    OUT,
     UNKNOWN_QUEUE_TYPE
 } queueType;
 
 class DccExInterface
 {
 private:
-    comStation      sta = _UNKNOWN_STA;               // needs to be set at init; defines which side this is running either CS or NW
-    comProtocol     comp = UNKNOWN_COM_PROTOCOL;      // sets the com protocol used between CS and NW; Only serial is supported right now 
-    HardwareSerial  *s;                               // valid only for Serial; this needs to be refacrored into subclasses 
-                                                      // instatated for specific protocols (maybe even at compile tme to reduce size)
-    uint32_t        speed;                           
-    bool            init = false;
-    bool            blocking = true;                  // send immediatly & wait for reply from the CS if false all commands 
-                                                      // send will be queued and handled in the loop 
-    uint64_t        seq = 0;
-    _tDccQueue      *incomming = nullptr;             // incomming queue holding message to be processed
-    _tDccQueue      *outgoing = nullptr;              // outgoing queue holding message to be send 
+    comStation sta = _UNKNOWN_STA;           // needs to be set at init; defines which side this is running either CS or NW
+    comProtocol comp = UNKNOWN_COM_PROTOCOL; // sets the com protocol used between CS and NW; Only serial is supported right now
+    HardwareSerial *s;                       // valid only for Serial; this needs to be refacrored into subclasses
+                                             // instatated for specific protocols (maybe even at compile tme to reduce size)
+    uint32_t speed;
+    bool init = false;
+    bool blocking = true; // send immediatly & wait for reply from the CS if false all commands
+                          // send will be queued and handled in the loop
+    uint64_t seq = 0;
+    static _tDccQueue incomming;
+    static _tDccQueue outgoing;
 
-    void write();                                     // writes the messages from the outgoing queue to the com protocol endpoint (Serial only
-                                                      // at this point
-    const char* csProtocolNames[8] = {"DCCEX", "WTH", "REPLY", "DIAG", "MQTT" , "HTTP", "CTRL", "UNKNOWN"};   //TODO move that to Progmem
-    const char* comStationNames[3] = {"CommandStation","NetworkStation","Unknown"};
-    
+    void write();                                                                                          // writes the messages from the outgoing queue to the com protocol endpoint (Serial only
+                                                                                                           // at this point
+    const char *csProtocolNames[8] = {"DCCEX", "WTH", "REPLY", "DIAG", "MQTT", "HTTP", "CTRL", "UNKNOWN"}; // TODO move that to Progmem
+    const char *comStationNames[3] = {"CommandStation", "NetworkStation", "Unknown"};
+
     HANDLERS;
     HANDLER_INIT;
 
@@ -148,40 +150,52 @@ public:
     const uint8_t recv_index = 0x34;
     const uint8_t send_index = 0x12;
 
-    auto getQueue(queueType q) -> _tDccQueue* {
-        switch(q) {
-         case IN : return incomming; break;
-         case OUT: return outgoing; break;
-         default : ERR(F("Unknown queue type returning null")); return nullptr;
+    auto getQueue(queueType q) -> _tDccQueue *
+    {
+        switch (q)
+        {
+        case IN:
+            return &incomming;
+            break;
+        case OUT:
+            return &outgoing;
+            break;
+        default:
+            ERR(F("Unknown queue type returning null"));
+            return nullptr;
         }
     }
     /**
      * @brief pushes a DccMessage struct into the designated queue
-     * 
-     * @param q : incomming or outgoing queue 
-     * @param packet : DccMessage struct to be pushed and send over the wire 
+     *
+     * @param q : incomming or outgoing queue
+     * @param packet : DccMessage struct to be pushed and send over the wire
      */
     void queue(queueType q, csProtocol p, DccMessage packet);
     void queue(uint16_t c, csProtocol p, char *msg);
-    void recieve();        // check the transport to see if tere is something for us
+    void recieve(); // check the transport to see if tere is something for us
     /**
-     * @brief setup the serial interface 
-     * 
+     * @brief setup the serial interface
+     *
      * @param *s        - pointer to a serial port. Default is Serial1 as Serial is used for monitor / upload etc..
      * @param speed     - default serial speed is 115200
      */
     void setup(HardwareSerial *s = &Serial1, uint32_t speed = 115200);
-    void setup(comStation station) {
-        sta = station;                  // sets to network or commandstation mode
+    void setup(comStation station)
+    {
+        sta = station; // sets to network or commandstation mode
         setup();
     }
     void loop();
-    auto size(queueType inout) -> size_t {
-        if (inout == IN) {
-            return incomming->size();
+    auto size(queueType inout) -> size_t
+    {
+        if (inout == IN)
+        {
+            return incomming.size();
         }
-        if (inout == OUT) {
-            return outgoing->size();
+        if (inout == OUT)
+        {
+            return outgoing.size();
         }
         ERR(F("Unknown queue in size; specifiy either IN or OUT"));
         return 0;
@@ -189,7 +203,7 @@ public:
     auto decode(csProtocol p) -> const char *;
     auto decode(comStation s) -> const char *;
 
-    DccExInterface(); 
+    DccExInterface();
     ~DccExInterface();
 };
 
